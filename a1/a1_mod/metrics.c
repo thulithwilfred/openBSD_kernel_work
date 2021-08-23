60d4d7773ad0afb91aff6fe325112543701bb490
Wilfred MK <wilfred@3301_BSD.home>
2021-08-24 00:08:01 +1000

    adding debug comments

--- a1/a1_mod/metrics.c
+++ a1/a1_mod/metrics.c
@@ -58,7 +58,7 @@ extern struct metrics_module_ops
     collect_pools_ops, collect_procs_ops, collect_disk_ops, collect_ipstat_ops;
 
 static struct metrics_module_ops *modops[] = {
-	&collect_ipstat_ops,
+	&collect_ipstat_ops, /* Adding ip metrics */
 	&collect_pf_ops,
 	&collect_cpu_ops,
 	&collect_if_ops,
