cbc797159822d26c911f58085c772f953224a598
Wilfred MK <wilfred@3301_BSD.home>
2021-09-15 09:21:15 +1000

    filling in kernel entry points

--- a2/a2_working/acct.c
+++ a2/a2_working/acct.c
@@ -12,13 +12,106 @@
 #include <sys/param.h>
 #include <sys/systm.h>
 #include <sys/conf.h>
+#include <sys/poll.h>
+
+#include <sys/filio.h>
+#include <sys/fcntl.h>
+
+#include <sys/rwlock.h>
 
 #include "acct.h"
+
+/** 
+ * TODO: 1. Parse the process hooks in here and set the appropriate structs and add them to the queue.
+ *		 2. Figure out where to put the kernel hooks for file auditing. 
+ */
+
+/* Local Defines */
+#define READ_ONLY 1 	/* Read Only Mode */
+
+struct rwlock rwl = RWLOCK_INITIALIZER("acct_lock");
+
+int ronly_flag = 0;
+
+/**
+ * @brief Initialise state required for operations 
+ * 
+ */
+int acctattach(int num) 
+{
+	return 0;
+}
 
 int
 acctopen(dev_t dev, int flag, int mode, struct proc *p)
 {
+	/* Allow only 0th minor to be opened */
+	if (minor(dev) != 0)
+		return (ENXIO);
+
+	/* If not opened exclusively */
+	if (!(flag & FFLAGS(O_EXCL)))
+		return EEXIST;
+
+	/* If not opened for reading or read/write */
+	if (!(flag & FFLAGS(O_RDONLY) || !(flag & FFLAGS(O_RDWR))))
+		return (EPERM);
+
+	if (flag & FFLAGS(O_RDONLY))
+		ronly_flag = READ_ONLY;
+		
+	return (0);
+}
+
+
+
+int 
+acctread(dev_t dev, struct uio *uio, int flags)
+{
 	return 0;
+}
+
+
+int 
+acctioctl(dev_t dev, u_long cmd, caddr_t data, int flag, struct proc *p)
+{
+	/* Support for generic ioctl requests */
+	switch(cmd) {
+		case FIONREAD:
+			/* Get the number of bytes that are immediately available for reading*/
+			//TODO: Get len of next struct in queue
+			*(int *)data = 0;
+			break;
+		case FIONBIO:
+			/*  Handled in the upper FS layer */
+			break;
+		case FIOASYNC:
+			return (EOPNOTSUPP);
+	}
+
+	/* Device specific ioctls not accepted in read only mode */
+	if (ronly_flag == READ_ONLY) 
+		return (ENOTTY);
+
+	/* Support for device specific ioctl requests */
+	switch(cmd) {
+		case ACCT_IOC_STATUS:
+			break;
+		case ACCT_IOC_FSTATUS:
+			break;
+		case ACCT_IOC_ENABLE:
+			break;
+		case ACCT_IOC_DISABLE:
+			break;
+		case ACCT_IOC_TRACK_FILE:
+			break;
+		case ACCT_IOC_UNTRACK_FILE:
+			break;
+		default:
+			/* Inappropriate ioctl for device */
+			return (ENOTTY);
+	}
+	return (0);
 }
 
 int
@@ -27,41 +120,26 @@ acctclose(dev_t dev, int flag, int mode, struct proc *p)
 	return 0;
 }
 
-int 
-acctread(dev_t dev, struct uio *uio, int flags)
-{
-	return 0;
-}
 
 int
 acctwrite(dev_t dev, struct uio *uio, int flags)
 {
-	return 0;	
+	return EOPNOTSUPP;	
 }
 
-
-int 
-acctioctl(dev_t dev, u_long cmd, caddr_t data, int flag, struct proc *p)
-{
-	return 0;
-}
 
 int 
 acctpoll(dev_t dev, int events, struct proc *p)
 {
-	return 0;
+	return POLLERR;
 }
 
 int
 acctkqfilter(dev_t dev, struct knote *kn)
 {
-	return 0;
+	return EOPNOTSUPP;
 }
 
-int acctattach(int num) 
-{
-	return 0;
-}
 
 void
 acct_fork(struct process *data) 
