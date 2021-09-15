ee068e13fba11ddabb1f789ef0628e9e11a133df
Wilfred MK <wilfred@3301_BSD.home>
2021-09-15 17:37:18 +1000

    added most features to acct_fork

--- a2/a2_working/acct.c
+++ a2/a2_working/acct.c
@@ -13,11 +13,14 @@
 #include <sys/systm.h>
 #include <sys/conf.h>
 #include <sys/poll.h>
-
+#include <sys/types.h>
+#include <sys/malloc.h>
 #include <sys/filio.h>
 #include <sys/fcntl.h>
+#include <sys/proc.h>
 
 #include <sys/rwlock.h>
+#include <sys/tty.h>
 
 #include "acct.h"
 
@@ -26,12 +29,42 @@
  *		 2. Figure out where to put the kernel hooks for file auditing. 
  */
 
+/* RW Lock */
+struct rwlock rwl = RWLOCK_INITIALIZER("acct_lock");
+
+/* TailQ Data Structures */
+
+/* 
+ * The following union allows for a single message to be enqueed into the message queue (TAILQ),
+ * the sender must set the type field of struct message and the receiver must read the type field of struct message to verify data types
+ */
+union message_data {
+	struct acct_fork fork_d;
+	struct acct_exec exec_d;
+	struct acct_exit exit_d;
+	struct acct_open open_d;
+	struct acct_rename rename_d;
+	struct acct_unlink unlink_d;
+	struct acct_close close_d;
+};
+
+
+struct message {
+	TAILQ_ENTRY(message) entries;
+	int type; 						/* Defines type of data the message is, i.e struct acct_fork relates to ACCT_MSG_FORK*/
+	unsigned int size; 				/* Total size of the data held by 'data' in bytes, set by sender */
+	union message_data data;		/* acct message */
+};
+
+TAILQ_HEAD(message_queue, message) head; 
+
 /* Local Defines */
 #define READ_ONLY 1 	/* Read Only Mode */
 
-struct rwlock rwl = RWLOCK_INITIALIZER("acct_lock");
 
+/* Globals */
 int ronly_flag = 0;
+int sequence_num = 0;
 
 /**
  * @brief Initialise state required for operations 
@@ -39,6 +72,8 @@ int ronly_flag = 0;
  */
 int acctattach(int num) 
 {
+	/* Initialise the message queue for acct messages */
+	TAILQ_INIT(&head);
 	return 0;
 }
 
@@ -59,7 +94,12 @@ acctopen(dev_t dev, int flag, int mode, struct proc *p)
 
 	if (flag & FFLAGS(O_RDONLY))
 		ronly_flag = READ_ONLY;
-		
+	
+	/* Reset Sequence Num */
+	rw_enter_write(&rwl); /* The lock is probably not needed here? */
+	sequence_num = 0;
+	rw_exit_write(&rwl);
+
 	return (0);
 }
 
@@ -114,6 +154,87 @@ acctioctl(dev_t dev, u_long cmd, caddr_t data, int flag, struct proc *p)
 	return (0);
 }
 
+void
+acct_fork(struct process *pdata) 
+{
+	struct acct_common common;
+	struct timespec uptime;
+	struct message *acct_msg;
+
+	/* Fork account not enabled, let's not worry about this... */
+	if(0)
+		//TODO
+		return;
+
+	/* Sequence begins from index 0 */
+	rw_enter_write(&rwl);
+	common.ac_seq = sequence_num;
+	sequence_num++;
+	rw_exit_write(&rwl);
+
+	acct_msg = malloc(sizeof(struct message), M_DEVBUF,M_WAITOK | M_ZERO);
+	
+	/* Set internal message data */
+	acct_msg->type = ACCT_MSG_FORK;
+	acct_msg->size = sizeof(struct acct_fork);
+
+	/* 
+	 * Create message common fields and update 'this' message. 
+	 * Enqueue the message, once required data fields are filled out 
+	 */
+	
+	/* Set msg type */
+	common.ac_type = ACCT_MSG_FORK;
+	
+	/* Set len (size bytes) */
+	common.ac_len = sizeof(struct acct_fork);
+
+	/*  Get command name */
+	memcpy(common.ac_comm, pdata->ps_comm, sizeof(common.ac_comm));
+
+	/* Get Time */
+	nanouptime(&uptime);
+	timespecsub(&uptime, &pdata->ps_start, &common.ac_etime); 	/* Calculate and update elapsed time*/
+	common.ac_btime = pdata->ps_start; 							/* Get Starting time */
+
+	/* Get Ids */
+	common.ac_pid = pdata->ps_ppid;								/* Referenced in process_new(), kern_fork.c */
+	common.ac_uid = pdata->ps_ucred->cr_uid;
+	common.ac_gid = pdata->ps_ucred->cr_gid;
+
+	/* Get Controlling TTY */
+	if ((pdata->ps_flags & PS_CONTROLT) && 
+		pdata->ps_pgrp->pg_session->s_ttyp)
+			common.ac_tty = pdata->ps_pgrp->pg_session->s_ttyp->t_dev;
+		else 
+			common.ac_tty = NODEV;
+	/* Get Accounting flags */
+	common.ac_flag = pdata->ps_acflag;
+
+	/* Update message common fields within this message */
+	acct_msg->data.fork_d.ac_common = common;
+	/* Set child pid */
+	acct_msg->data.fork_d.ac_cpid = pdata->ps_pid; 				 /* Referenced in process_new(), kern_fork.c */
+
+	/* Enqueue Data */
+	rw_enter_write(&rwl);
+	TAILQ_INSERT_TAIL(&head, acct_msg, entries);
+	rw_exit_write(&rwl);
+} 
+
+void
+acct_exec(struct process *data)
+{
+
+}
+
+void
+acct_exit(struct process *data)
+{
+	
+}
+
+
 int
 acctclose(dev_t dev, int flag, int mode, struct proc *p)
 {
@@ -141,20 +262,3 @@ acctkqfilter(dev_t dev, struct knote *kn)
 }
 
 
-void
-acct_fork(struct process *data) 
-{
-
-}
-
-void
-acct_exec(struct process *data)
-{
-
-}
-
-void
-acct_exit(struct process *data)
-{
-	
-}
