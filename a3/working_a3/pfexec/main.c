99c6ff4d8f6d50f64d2b349e79ad91a723e8cbc6
Wilfred MK <wilfred@3301_BSD.home>
2021-10-29 16:05:20 +1000

    styled

--- a3/working_a3/pfexec/main.c
+++ a3/working_a3/pfexec/main.c
@@ -1,26 +1,26 @@
 /*
-* COMP3301 - Assingment 3
-*
-* pfexec is a commandline utility that processes it's arguments and calls the pfexecve() system call.
-* 
-* Author	: Wilfred MK
-* SID		: S4428042
-* Riv		: 0.1
-* Last Updated	: 12/10/2021
-*
-* THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
-* ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-* ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
-* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
-* OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-* HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-* LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
-* SUCH DAMAGE.
-*
-* @(#)main.c v0.1 (UQ) - Wilfred MK
-*/
+ * COMP3301 - Assingment 3
+ *
+ * pfexec is a commandline utility that processes it's arguments and calls the
+ * pfexecve() system call.
+ * Author	: Wilfred MK
+ * SID		: S4428042
+ * Riv		: 0.1
+ * Last Updated	: 12/10/2021
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * SUCH DAMAGE.
+ *
+ * @(#)main.c v0.1 (UQ) - Wilfred MK
+ */
 
 
 #include <sys/param.h>
@@ -46,7 +46,7 @@ struct pfexec_data {
 	char executable_name[PATH_MAX];
 	char username[LOGIN_NAME_MAX];
 	char **args;
-	uint32_t args_count; //To be used when freeing.
+	uint32_t args_count;  /* To be used when freeing. */
 };
 
 /* Define errors and errvals used in pfexec */
@@ -59,32 +59,30 @@ enum pfexec_errno
 	E_FAILED_PFEXEC = 5
 };
 
-/**
-* @brief Prints the program usage message and exits.
-* 
-*/
+/*
+ * @brief Prints the program usage message and exits.
+ */
 __dead static void
-usage(void) 
+usage(void)
 {
-	fprintf(stderr, "Usage: pfexec [options] <executable> [args...] \npfexec" 
-	    "[options] -s\n\n");
+	fprintf(stderr, "Usage: pfexec [options] <executable> [args...]\n"
+	"pfexec[options] -s\n\n");
 	fprintf(stderr, "Options:\n");
-	fprintf(stderr, "\t-u user\tAssume the privileges of the given user instead"
-	     "of \"root\"\n");
-	fprintf(stderr, "\t-n\tNon-iteractive/batch mode: no prompts are used\n\t\t"
-	    "if prompts are required, will fail\n");
+
+	fprintf(stderr, "\t-u user\tAssume the privileges of the given"
+	    " user instead of \"root\"\n");
+	fprintf(stderr, "\t-n\tNon-iteractive/batch mode:"
+	" no prompts are used\n\t\t if prompts are required, will fail\n");
 	fprintf(stderr, "\t-s\tExecute a shell, rather than a specific command,"
 	    " no options\n");
-
 	exit(E_BADARGS);
 }
 
-/**
-* @brief Free allocated memory for pfexec arguments
-* 
-*/
+/*
+ * Free allocated memory for pfexec arguments
+ */
 void
-free_args(char** args, uint32_t num_elements)
+free_args(char **args, uint32_t num_elements)
 {
 	for (int i = 0; i < num_elements; ++i) {
 		free(args[i]);
@@ -92,35 +90,29 @@ free_args(char** args, uint32_t num_elements)
 	free(args);
 }
 
-/**
-* @brief Set PF flags based on arguments to this programs
-* @note  PF flags are defined in include/sys/pfexec.h
-* 
-* @param opt_flag argument flags to this program
-* @return set PF flags
-*/
-uint32_t gen_pfo_flags(uint32_t opt_flag)
+/*
+ * Set PF flags based on arguments to this programs
+ * @note  PF flags are defined in include/sys/pfexec.h
+ */
+uint32_t
+gen_pfo_flags(uint32_t opt_flag)
 {
 	uint32_t pfo_flag = 0;
 
 	if (opt_flag & PFEXEC_N)
 		pfo_flag |= PFEXECVE_NOPROMPT;
 
-	if (opt_flag & PFEXEC_U) 
+	if (opt_flag & PFEXEC_U)
 		pfo_flag |= PFEXECVE_USER;
 
 	return pfo_flag;
 }
 
-/**
-* @brief Execute a shell based on SHELL env variable, else run /bin/ksh.
-*		 	
-* 
-* @param opt_flag set privileges and interactive mode of the shell
-* @return 0 on success or errno
-*/
-int 
-execute_shell(uint32_t opt_flag, struct pfexec_data *d) 
+/*
+ * Execute a shell based on SHELL env variable, else run /bin/ksh.
+ */
+int
+execute_shell(uint32_t opt_flag, struct pfexec_data *d)
 {
 	struct pfexecve_opts p_opts;
 	int err_val = 0, i;
@@ -130,7 +122,7 @@ execute_shell(uint32_t opt_flag, struct pfexec_data *d)
 	args[1] = malloc(sizeof(char) * 12);
 
 	/* Evaluate SHELL env variable */
-	if (getenv("SHELL") == NULL ) 
+	if (getenv("SHELL") == NULL)
 		strcpy(shell_path, "/bin/ksh");
 	else
 		strcpy(shell_path, getenv("SHELL"));
@@ -139,12 +131,12 @@ execute_shell(uint32_t opt_flag, struct pfexec_data *d)
 	strncpy(args[0], shell_path, 512);
 	args[1] = NULL;
 
-	p_opts.pfo_flags = gen_pfo_flags(opt_flag);	
+	p_opts.pfo_flags = gen_pfo_flags(opt_flag);
 
 	/* Username defined, copy in uname */
-	if (opt_flag & PFEXEC_U) 
+	if (opt_flag & PFEXEC_U)
 		strcpy(p_opts.pfo_user, d->username);
-		
+
 	/* Call to pfexecve */
 	err_val = pfexecvp(&p_opts, shell_path, args);
 
@@ -158,13 +150,9 @@ execute_shell(uint32_t opt_flag, struct pfexec_data *d)
 	return err_val;
 }
 
-/**
-* @brief Process pfexec based on the provided options
-* 
-* @param opt_flag options
-* @param d holds executable and exec args
-* @return 0 on success, else errno
-*/
+/*
+ * Process pfexec based on the provided options
+ */
 int
 process_pfexec(uint32_t opt_flag, struct pfexec_data *d)
 {
@@ -175,11 +163,11 @@ process_pfexec(uint32_t opt_flag, struct pfexec_data *d)
 	if (opt_flag & PFEXEC_S)
 		return execute_shell(opt_flag, d);
 
-	p_opts.pfo_flags = gen_pfo_flags(opt_flag);	
+	p_opts.pfo_flags = gen_pfo_flags(opt_flag);
 
 	/* Username defined, copy in uname */
-	if (opt_flag & PFEXEC_U) 		
-		strcpy(p_opts.pfo_user, d->username);	
+	if (opt_flag & PFEXEC_U)
+		strcpy(p_opts.pfo_user, d->username);
 
 	/* Call to pfexecve */
 	err_val = pfexecvp(&p_opts, d->executable_name, d->args);
@@ -188,14 +176,12 @@ process_pfexec(uint32_t opt_flag, struct pfexec_data *d)
 	return (err_val);
 }
 
-/**
-* @brief pfexec is a commandline utility that processes
-*		 it's arguments and calls the pfexecve() system call.
-* 
-* @return int 0 on success or errno
-*/
+/*
+ * pfexec is a commandline utility that processes
+ *		 it's arguments and calls the pfexecve() system call.
+ */
 int
-main(int argc, char** argv) 
+main(int argc, char **argv)
 {
 	struct pfexec_data *d;
 	int ch, errno = 0;
@@ -203,12 +189,12 @@ main(int argc, char** argv)
 
 	if (argc <= 1)
 		usage();
-	
+
 	d = malloc(sizeof(struct pfexec_data));
 	bzero(d, sizeof(struct pfexec_data));
 
-	while((ch = getopt(argc, argv, "u:ns")) != -1) {
-		switch(ch) {
+	while ((ch = getopt(argc, argv, "u:ns")) != -1) {
+		switch (ch) {
 		case 'u':
 			strcpy(d->username, optarg);
 			opt_flag |= PFEXEC_U;
@@ -234,7 +220,7 @@ main(int argc, char** argv)
 		/* Should not have any remaining args */
 		if (argc != 0)
 			usage();
-		
+
 		errno = execute_shell(opt_flag, d);
 
 		if (errno)
@@ -242,7 +228,7 @@ main(int argc, char** argv)
 	}
 
 	if (argc == 0)
-		usage(); 
+		usage();
 
 	/* Copy in executable name */
 	if (strlen(*argv) > EXE_MAXLEN)
@@ -250,19 +236,19 @@ main(int argc, char** argv)
 
 	strlcpy(d->executable_name, *argv, PATH_MAX);
 
-	/* 
-		*	Arguments Array 
-		*	argc here holds prog name and remaining args, +1 to add NUL 
-		*	terminate.
-		*/
-	d->args =  malloc(sizeof(char*) * ((argc) + 1));
+	/*
+	 *	Arguments Array
+	 *	argc here holds prog name and remaining args, +1 to add NUL
+	 *	terminate.
+	 */
+	d->args = malloc(sizeof(char *) * ((argc) + 1));
 
 	/* Args first element should be progname */
 	d->args[0] = malloc(sizeof(char) * strlen(*argv));
 	strlcpy(d->args[0], *argv, EXE_MAXLEN);
 
 	/* Create an args list */
-	for(int i = 1; i < argc ; ++i) {
+	for (int i = 1; i < argc; ++i) {
 		if (strlen(argv[i]) > ARG_MAX)
 			return E_TOOLONG;
 		d->args[i] = malloc(sizeof(char) * strlen(argv[i]));
@@ -271,7 +257,7 @@ main(int argc, char** argv)
 	}
 
 	/* Null terminate args */
-	d->args[d->args_count + 1] = (char*)0; 
+	d->args[d->args_count + 1] = (char *)0;
 
 	errno = process_pfexec(opt_flag, d);
 
@@ -283,5 +269,5 @@ early_close:
 	if (errno)
 		err(errno, "pfexecvp");
 
-	return(0);
+	return (0);
 }
